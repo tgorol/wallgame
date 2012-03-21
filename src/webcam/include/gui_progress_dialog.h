454aec0809954973efbdbdf621d09f465ebc9b32
Tomasz Gorol <tomasz.gorol@gmail.com>
2012-03-21 20:29:02 +0000

    Gui_progress dialog code restructured

--- src/webcam/include/gui_progress_dialog.h
+++ src/webcam/include/gui_progress_dialog.h
@@ -2,14 +2,21 @@
 #define _GUI_PROGRESS_DIALOG_H
 
 
-typedef wg_boolean (*action_cb)(void *data);
+typedef enum Gui_progress_action{
+    GUI_PROGRESS_BACK   =   0,
+    GUI_PROGRESS_NEXT        ,
+    GUI_PROGRESS_ENTER       ,
+    GUI_PROGRESS_LEAVE
+}Gui_progress_action;
+
+typedef wg_boolean (*action_cb)(Gui_progress_action button, void *data);
 
 typedef struct Gui_progress_dialog_screen Gui_progress_dialog_screen;
 
 typedef struct Gui_progress_dialog Gui_progress_dialog;
 
 WG_PUBLIC Gui_progress_dialog_screen *
-gui_progress_dialog_screen_new(action_cb next, action_cb prev, void *user_data, 
+gui_progress_dialog_screen_new(action_cb next, void *user_data,
         const wg_char *text);
 
 WG_PUBLIC void
